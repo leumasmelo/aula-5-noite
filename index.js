e397178dc0627f79d742562e3f1f0a1824569c3a
leumasmelo <142456194+leumasmelo@users.noreply.github.com>
2023-08-21 21:44:57 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+*Samuel Melo
+*Primeiro comentario
+
+Quando pensamos que sabemos alguma coisa da vida, vem a verdade e muda  todas as nossas perguntas
+*/console.log("olá mundo!");
+//prompt("Digite seu nome");
+
+//Variáveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB)
+let numero = 50;
+console.log(numero);
+numero = 80;
+//const nome = "Samuel";
+//const idade = 35;
+//console.log( "Olá!", "Meu nome é ", nome, "e eu tenho", idade, "anos");
+let nomeExercício = 2;
+const nome = "Leumas";
+const sobrenome = "Melo";
+const idade = 35;
+let estudante = true;
+console.log( "Olá!", "Meu nome é", nome, sobrenome, "e eu tenho", idade, "anos", "se sou um estudante");
+
+
+
